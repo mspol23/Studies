708c5cbee76e831361ff97bd0c98deb05c1ffaee
Marcos Oliveira <mspol23@gmail.com>
2023-02-17 21:34:13 -0300

    coding first app with Node. creating local host.

--- Especializar/API/ProjetoAPI/index.js
+++ Especializar/API/ProjetoAPI/index.js
@@ -1,3 +1,7 @@
 const exrpess = require('express')
 const app = exrpess()
 app.listen('3000')
+
+app.route('/').get( (req,res) => res.send('Helo! <a href="/sobre"> link </a>') )
+
+app.route('/sobre').get( (req,res) => res.send('Helo! Sobre!') )
